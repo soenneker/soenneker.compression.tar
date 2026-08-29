3b31f87a01dcd3fa0a379dfd38e1b22f55c1ed5b
soenneker <jake@soenneker.com>
2026-08-29 14:16:57 -0500

    Improve public API XML documentation

--- src/Soenneker.Compression.Tar/Registrars/TarUtilRegistrar.cs
+++ src/Soenneker.Compression.Tar/Registrars/TarUtilRegistrar.cs
@@ -12,6 +12,8 @@ public static class TarUtilRegistrar
     /// <summary>
     /// Adds <see cref="ITarUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddTarUtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<ITarUtil, TarUtil>();
@@ -22,6 +24,8 @@ public static class TarUtilRegistrar
     /// <summary>
     /// Adds <see cref="ITarUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddTarUtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<ITarUtil, TarUtil>();
